5b43f1d2358726369250741263b2bb7980435dd8
Jennings <zjennings@dhcp-169-233-175-232.cruznet.ucsc.edu>
2017-01-17 10:27:18 -0800

    initial commit of n3115 work

--- fakeStars.py
+++ fakeStars.py
@@ -4,19 +4,20 @@ import os
 import pickle
 import astropy.coordinates as coords
 import astropy.units as u
-from sklearn.svm import SVC as svc
-from sklearn.preprocessing import StandardScaler
-from sklearn.cross_validation import StratifiedShuffleSplit
-from sklearn.grid_search import GridSearchCV
+#from sklearn.svm import SVC as svc
+#from sklearn.preprocessing import StandardScaler
+#from sklearn.cross_validation import StratifiedShuffleSplit
+#from sklearn.grid_search import GridSearchCV
 from astropy.wcs import WCS
 from astropy.io import fits
+import seWrapper as se
 
 from pyraf.iraf import artdata
 
 
 class fakeStarTests(object):
     def __init__(self,image,phot_catalog,pix_scale=0.2,gain=1.0,rd_noise=0.0,\
-        zpt=30.,seeing=3.5,config_file='foo.sex',write_true_phot=False):
+        zpt=30.,seeing=3.5,config_file='foo.sex',write_true_phot=False,exptime=1.,background=0.):
         #self.runner = seRunner()
         self.phot = phot_catalog
         self.name = image
@@ -30,6 +31,8 @@ class fakeStarTests(object):
         self.config_file=config_file
         self.wcs = WCS(image)
         self.write_true_phot = write_true_phot
+        self.exptime = exptime
+        self.background = background
 
         if self.write_true_phot:
             self.true_mags,self.true_x,self.true_y = self.unpackPhotCatalog(self.phot)
@@ -44,7 +47,7 @@ class fakeStarTests(object):
         Use pyraf artdata.mkobjects to place the artificial stars in the image
         '''
         artdata.mkobjects(input=name,output=output,objects=objects,\
-                        magzero=self.zpt,gain=self.gain,rdnoise=self.rd_noise,radius=self.seeing,background=0.0)
+                        magzero=self.zpt,gain=self.gain,rdnoise=self.rd_noise,radius=self.seeing,background=self.background,exptime=self.exptime)
 
 
     def makeStarList(self,name,n_stars=1000,min_mag=24,max_mag=28,x_range=(0,1000),y_range=(0,1000),write_true_phot=False):
@@ -55,8 +58,8 @@ class fakeStarTests(object):
         #    minmag=min_mag,maxmag=max_mag,luminosity='uniform',lseed='INDEF')
 
         mags = np.random.uniform(low=min_mag,high=max_mag,size=n_stars)
-        x = np.random.uniform(high=self.header['naxis1'],size=n_stars)
-        y = np.random.uniform(high=self.header['naxis2'],size=n_stars)
+        x = np.random.uniform(low=x_range[0],high=x_range[1],size=n_stars)
+        y = np.random.uniform(low=y_range[0],high=y_range[1],size=n_stars)
 
         fake_cat = np.vstack([x,y,mags])
 
@@ -85,10 +88,17 @@ class fakeStarTests(object):
         #return the fake catalogs so that we know what to check against
         return (x,y,mags)
 
-    def genFakeCatalogs(self,n_iter=1,min_mag=22.,max_mag=26.,n_stars=1000,overwrite=False):
+    def genFakeCatalogs(self,n_iter=1,min_mag=22.,max_mag=26.,n_stars=1000,overwrite=False,xmax=None,ymax=None,xmin=0.,ymin=0.,match_rad=0.5*u.arcsec):
         '''
         Perform photometry on the fake catalogs
         '''
+        if xmax is None:
+            xmax = self.header['naxis1']
+            
+        if ymax is None:
+            ymax = self.header['naxis2']
+             
+        
         if overwrite or not hasattr(self,'fake_cats'):
             self.fake_cats = dict()
             self.fake_cats_x = dict()
@@ -105,7 +115,8 @@ class fakeStarTests(object):
             sp.call('rm '+fake_image,shell=True)
 
 
-            starlist_x,starlist_y,starlist_mags = self.makeStarList(starlist_name,min_mag=min_mag,max_mag=max_mag,n_stars=n_stars)
+            starlist_x,starlist_y,starlist_mags = self.makeStarList(starlist_name,min_mag=min_mag,max_mag=max_mag,n_stars=n_stars,\
+                x_range=(xmin,xmax),y_range=(ymin,ymax))
             starlist_mags={'input':starlist_mags}
             starlist_fwhm = {'input':np.zeros(starlist_x.size)+99.999}
             starlist_ellipticity = {'input':np.zeros(starlist_x.size)+99.999}
@@ -118,18 +129,18 @@ class fakeStarTests(object):
             starlist_coords = coords.SkyCoord(ra=starlist_ra*u.degree,\
                 dec=starlist_dec*u.degree)
 
-            self.starlist_cat = seCatalog()
+            self.starlist_cat = se.seCatalog()
             self.starlist_cat.coords = starlist_coords
             self.starlist_cat.mags = starlist_mags
             self.starlist_cat.fwhm = starlist_fwhm
             self.starlist_cat.ellipticity=starlist_ellipticity
 
             self.runSE(fake_image,self.config_file,cat_name=recovered_cat_name)
-            self.recovered_cat = seCatalog()
+            self.recovered_cat = se.seCatalog()
             self.recovered_cat.create_new_catalog_file(recovered_cat_name,filter='recovered')
 
-            self.starlist_cat.mergeCatalog(self.recovered_cat)
-            self.starlist_cat.mergeCatalog(self.phot)
+            self.starlist_cat.mergeCatalog(self.recovered_cat,match_rad=match_rad)
+            self.starlist_cat.mergeCatalog(self.phot,match_rad=match_rad)
 
             #create new photometry lists containing only the new stars
             old_phot = self.starlist_cat.mags['input'] > 99
@@ -147,12 +158,12 @@ class fakeStarTests(object):
             self.fake_cats_y[key] = input_pix_y
 
             un_rec = recovered_mags > 99
-            self.unrec_cat=seCatalog()
+            self.unrec_cat= se.seCatalog()
             self.unrec_cat.coords = input_coords[un_rec]
             self.unrec_cat.id = np.zeros(un_rec.size)
             self.unrec_cat.mags = {'input':input_mags[un_rec]}
 
-            self.rec_cat=seCatalog()
+            self.rec_cat= se.seCatalog()
             self.rec_cat.coords = input_coords[~un_rec]
             self.rec_cat.id = np.zeros(un_rec.size)
             self.rec_cat.mags = {'input':input_mags[~un_rec]}
